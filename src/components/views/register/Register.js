4ac03eb26042917af34e506eea9ee06cd5a19ae8
Mariano <marianod9797@gmail.com>
2023-03-31 18:05:54 -0300

    cambie el register y agregue validaciones

--- src/components/views/register/Register.js
+++ src/components/views/register/Register.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import axios from "../../../config/axiosInit";
 import emailjs from '@emailjs/browser';
+import "./register.css"
 
 
 const Register = ({ setLoggedUser }) => {
@@ -12,22 +13,51 @@ const Register = ({ setLoggedUser }) => {
   const [errorMessage, setErrorMessage] = useState('');
   const URL = process.env.REACT_APP_API_HAMBURGUESERIA_USUARIO
 
+
+  const handleResetValidation = () => {
+    form.current.classList.remove('was-validated');
+  };
+
+
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
+
+    handleResetValidation();
+
+    if (name === "name" && /[^a-zA-Z\s]/.test(value)) {
+      setErrorMessage("No se permiten números o símbolos como nombre de usuario");
+    } else {
+      setErrorMessage("");
+    }
+
     setInputs((values) => ({ ...values, [name]: value }));
   };
+
   //useNavigate
   const navigate = useNavigate();
 
-  
+
   const form = useRef();
 
   //Funcion para crear el producto
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(inputs);
-    //Valido los campos
+
+    //validar los campos usando las validaciones de Bootstrap
+    const isValidForm = form.current.checkValidity();
+    form.current.classList.add('was-validated');
+
+    if (!isValidForm) {
+      return;
+
+    }
+    if (inputs.password !== inputs.passwordrep) {
+      setErrorMessage("Las contraseñas no coinciden");
+      setError(true);
+      return;
+    }
 
     //Envio los datos para guardarlos
     const newUser = {
@@ -39,19 +69,12 @@ const Register = ({ setLoggedUser }) => {
       activo: true
     };
     try {
-      /* const res = await fetch(`${URL}/register`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newUser),
-          }); */
       const res = await axios.post(`${URL}/register`, newUser);
       console.log(res);
       if (res.status === 201) {
         Swal.fire("Created!", "Your user has been created.", "success");
         // const data = await res.json(); // si es con fetch
-        const data = res.data 
+        const data = res.data
         console.log(data);
         localStorage.setItem("user-token", JSON.stringify(data));
         setLoggedUser(data);
@@ -62,42 +85,46 @@ const Register = ({ setLoggedUser }) => {
       setError(true);
       error.response.data?.message && setErrorMessage(error.response.data?.message)
     }
-    
+
     emailjs.sendForm('service_470nr1h', 'template_q5eze0c', form.current, 'SFzC0PALs3luZR9uq')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
 
   };
 
   return (
     <div>
-      <Container className="py-5">
-        <h1>Register</h1>
+      <Container className="registerContainer py-5">
+        <h1 className="text-center">Register</h1>
         <hr />
         <Form className="my-5" onSubmit={handleSubmit} ref={form}>
-          <Form.Group className="mb-3" controlId="formBasicUserName">
-            <Form.Label>User name*</Form.Label>
+          <Form.Group controlId="formBasicUserName">
+            <Form.Label>User name <span>*</span></Form.Label>
             <Form.Control
               type="text"
-              placeholder="Ej: John Doe"
-              minLength= '5'
-              maxLength='20'
+              placeholder="Ej: Jose Paz"
+              minLength="5"
+              maxLength="40"
               name="name"
               required
               value={inputs.name || ""}
               onChange={(e) => handleChange(e)}
+              isInvalid={inputs.name && /[^a-zA-Z\s]/.test(inputs.name)}
             />
+            <Form.Control.Feedback type="invalid">
+              No se permiten números o simbolos como nombre de usuario
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email*</Form.Label>
             <Form.Control
-              type="text"
-              placeholder="johndoe@gmail.com"
-              minLength= '5'
-              maxLength='30'
+              type="email"
+              placeholder="jose@gmail.com"
+              minLength='5'
+              maxLength='45'
               name="email"
               required
               value={inputs.email || ""}
@@ -109,7 +136,7 @@ const Register = ({ setLoggedUser }) => {
             <Form.Control
               type="password"
               placeholder="Ej: Ingrese su password"
-              minLength= '5'
+              minLength='5'
               maxLength='18'
               name="password"
               required
@@ -123,28 +150,35 @@ const Register = ({ setLoggedUser }) => {
             <Form.Control
               type="password"
               placeholder="Ej: Repeat your password"
-              minLength= '5'
+              minLength='5'
               maxLength='18'
               name="passwordrep"
               required
               value={inputs.passwordrep || ""}
               onChange={(e) => handleChange(e)}
             />
+            {inputs.passwordrep &&
+              inputs.password !== inputs.passwordrep && (
+                <Form.Control.Feedback type="invalid">
+                  Las contraseñas no coinciden
+                </Form.Control.Feedback>
+              )}
+
           </Form.Group>
 
+          <div className="text-center">
+            <button className="btnRegister">Registrarse</button>
+          </div>
 
-          <Link to="/auth/login" className="btn btn-info text-decoration-none">
+          <Link to="/auth/login" className="backHomeLink">
             Back to login
           </Link>
-          <div className="text-center">
-            <button className="btn-primary">Send</button>
-          </div>
         </Form>
         {error ? (
-        <Alert variant="danger" onClick={() => setError(false)} dismissible>
-          {errorMessage}
-        </Alert>
-      ) : null}
+          <Alert variant="danger" onClick={() => setError(false)} dismissible>
+            {errorMessage}
+          </Alert>
+        ) : null}
       </Container>
     </div>
   );
