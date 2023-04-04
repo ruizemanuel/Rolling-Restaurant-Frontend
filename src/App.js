d697998e985630c1872f1f93c49c1ff44dc5d2e9
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-04-04 19:57:09 -0300

    solucion a redireccion a Error 404 cuando se ingresa a una ruta no definida

--- src/App.js
+++ src/App.js
@@ -27,9 +27,9 @@ function App() {
   const [products, setProducts] = useState([]);
   const [loggedUser, setLoggedUser] = useState(localStorage.getItem('user-token') ? JSON.parse(localStorage.getItem("user-token")) : {});
   const URL = process.env.REACT_APP_API_HAMBURGUESERIA;
-  
-  
-  
+
+
+
 
 
   console.log('LOGGED DESDE APP', loggedUser)
@@ -41,8 +41,8 @@ function App() {
 
   const getApi = async () => {
     try {
-  
-     const res = await axios.get(URL);
+
+      const res = await axios.get(URL);
       const productApi = res?.data;
 
       setProducts(productApi);
@@ -51,11 +51,6 @@ function App() {
     }
   };
 
- 
-
-
-
-  
 
 
   return (
@@ -67,72 +62,90 @@ function App() {
             <Route exact path="/" element={<Home products={products} />} />
             <Route
               exact
-              path="/*"
+              path="/product/table"
               element={
                 <ProtectedRoute loggedUser={loggedUser}>
-                  <Routes>
-                    <Route
-                      exact
-                      path="/product/table"
-                      element={
-                        <ProductsTable
-                          products={products}
-                          URL={URL}
-                          getApi={getApi}
-                        />
-                      }
-                    />
-                    <Route
-                      exact
-                      path="/product/create"
-                      element={<ProductCreate URL={URL} getApi={getApi} />}
-                    />
-                    <Route
-                      exact
-                      path="/product/edit/:id"
-                      element={<ProductEdit URL={URL} getApi={getApi} />}
-                    />
-
-                    <Route
-                      exact
-                      path="/user/table"
-                      element={
-                        <UsersTable/>
-                      }
-                    />
-
-                    <Route
-                      exact
-                      path="/user/create"
-                      element={<UserCreate/>}
-                    />
-
-                    <Route
-                      exact
-                      path="/user/edit/:id"
-                      element={<UserEdit/>}
-                    />
-
-                    <Route
-                      exact
-                      path="/pedidos/table"
-                      element={
-                        <PedidosTableAdmin/>
-                      }
-                    />
-
-                    <Route
-                      exact
-                      path="/pedido/edit/:id"
-                      element={
-                        <PedidoAdminEdit/>
-                      }
-                    />
-
-                  </Routes>
+                  <ProductsTable
+                    products={products}
+                    URL={URL}
+                    getApi={getApi}
+                  />
                 </ProtectedRoute>
               }
             />
+
+            <Route
+              exact
+              path="/product/create"
+              element={
+                <ProtectedRoute loggedUser={loggedUser}>
+                  <ProductCreate URL={URL} getApi={getApi} />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              exact
+              path="/product/edit/:id"
+              element={
+                <ProtectedRoute loggedUser={loggedUser}>
+                  <ProductEdit URL={URL} getApi={getApi} />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              exact
+              path="/user/table"
+              element={
+                <ProtectedRoute loggedUser={loggedUser}>
+                  <UsersTable />
+                </ProtectedRoute>
+              }
+            />
+
+
+            <Route
+              exact
+              path="/user/create"
+              element={
+                <ProtectedRoute loggedUser={loggedUser}>
+                  <UserCreate />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              exact
+              path="/user/edit/:id"
+              element={
+                <ProtectedRoute loggedUser={loggedUser}>
+                  <UserEdit />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              exact
+              path="/pedidos/table"
+              element={
+                <ProtectedRoute loggedUser={loggedUser}>
+                  <PedidosTableAdmin />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              exact
+              path="/pedido/edit/:id"
+              element={
+                <ProtectedRoute loggedUser={loggedUser}>
+                  <PedidoAdminEdit />
+                </ProtectedRoute>
+              }
+            />
+
+
             <Route
               exact
               path="/product/buy/:id"
@@ -141,7 +154,7 @@ function App() {
             <Route
               exact
               path="/pedidos"
-              element={<PedidosTable/>}
+              element={<PedidosTable />}
             />
             <Route
               exact
@@ -155,8 +168,249 @@ function App() {
         {/* <enviarmail/> */}
         <Footer />
       </BrowserRouter>
-    </div>
+    </div >
   );
+
+
+
+
+
 }
 
 export default App;
+
+
+
+
+
+
+
+// return (
+//   <div>
+//     <BrowserRouter>
+//       <Navigation loggedUser={loggedUser} setLoggedUser={setLoggedUser} />
+//       <main>
+//         <Routes>
+//           <Route exact path="/" element={<Home products={products} />} />
+//           <Route
+//             exact
+//             path="/*"
+//             element={
+//               <ProtectedRoute loggedUser={loggedUser}>
+//                 <Routes>
+//                   <Route
+//                     exact
+//                     path="/product/table"
+//                     element={
+//                       <ProductsTable
+//                         products={products}
+//                         URL={URL}
+//                         getApi={getApi}
+//                       />
+//                     }
+//                   />
+//                   <Route
+//                     exact
+//                     path="/product/create"
+//                     element={<ProductCreate URL={URL} getApi={getApi} />}
+//                   />
+//                   <Route
+//                     exact
+//                     path="/product/edit/:id"
+//                     element={<ProductEdit URL={URL} getApi={getApi} />}
+//                   />
+
+//                   <Route
+//                     exact
+//                     path="/user/table"
+//                     element={
+//                       <UsersTable/>
+//                     }
+//                   />
+
+//                   <Route
+//                     exact
+//                     path="/user/create"
+//                     element={<UserCreate/>}
+//                   />
+
+//                   <Route
+//                     exact
+//                     path="/user/edit/:id"
+//                     element={<UserEdit/>}
+//                   />
+
+//                   <Route
+//                     exact
+//                     path="/pedidos/table"
+//                     element={
+//                       <PedidosTableAdmin/>
+//                     }
+//                   />
+
+//                   <Route
+//                     exact
+//                     path="/pedido/edit/:id"
+//                     element={
+//                       <PedidoAdminEdit/>
+//                     }
+//                   />
+
+//                 </Routes>
+//               </ProtectedRoute>
+//             }
+//           />
+//           <Route
+//             exact
+//             path="/product/buy/:id"
+//             element={<ProductDetails URL={URL} />}
+//           />
+//           <Route
+//             exact
+//             path="/pedidos"
+//             element={<PedidosTable/>}
+//           />
+//           <Route
+//             exact
+//             path="/auth/login/"
+//             element={<Login setLoggedUser={setLoggedUser} />}
+//           />
+//           <Route exact path="/auth/register/" element={<Register setLoggedUser={setLoggedUser} />} />
+//           <Route exact path="*" element={<Error404 />} />
+//         </Routes>
+//       </main>
+//       {/* <enviarmail/> */}
+//       <Footer />
+//     </BrowserRouter>
+//   </div>
+// );
+
+
+
+
+
+
+
+
+
+
+// return (
+//   <div>
+//     <BrowserRouter>
+//       <Navigation loggedUser={loggedUser} setLoggedUser={setLoggedUser} />
+//       <main>
+//         <Routes>
+//           <Route exact path="/" element={<Home products={products} />} />
+//           <Route
+//             exact
+//             path="/product/table"
+//             element={
+//               <ProtectedRoute loggedUser={loggedUser}>
+//                 <ProductsTable
+//                   products={products}
+//                   URL={URL}
+//                   getApi={getApi}
+//                 />
+//               </ProtectedRoute>
+//             }
+//           />
+
+//           <Route
+//             exact
+//             path="/product/create"
+//             element={
+//               <ProtectedRoute loggedUser={loggedUser}>
+//                 <ProductCreate URL={URL} getApi={getApi} />
+//               </ProtectedRoute>
+//             }
+//           />
+
+//           <Route
+//             exact
+//             path="/product/edit/:id"
+//             element={
+//               <ProtectedRoute loggedUser={loggedUser}>
+//                 <ProductEdit URL={URL} getApi={getApi} />
+//               </ProtectedRoute>
+//             }
+//           />
+
+//           <Route
+//             exact
+//             path="/user/table"
+//             element={
+//               <ProtectedRoute loggedUser={loggedUser}>
+//                 <UsersTable />
+//               </ProtectedRoute>
+//             }
+//           />
+
+
+//           <Route
+//             exact
+//             path="/user/create"
+//             element={
+//               <ProtectedRoute loggedUser={loggedUser}>
+//                 <UserCreate />
+//               </ProtectedRoute>
+//             }
+//           />
+
+//           <Route
+//             exact
+//             path="/user/edit/:id"
+//             element={
+//               <ProtectedRoute loggedUser={loggedUser}>
+//                 <UserEdit />
+//               </ProtectedRoute>
+//             }
+//           />
+
+//           <Route
+//             exact
+//             path="/pedidos/table"
+//             element={
+//               <ProtectedRoute loggedUser={loggedUser}>
+//                 <PedidosTableAdmin />
+//               </ProtectedRoute>
+//             }
+//           />
+
+//           <Route
+//             exact
+//             path="/pedido/edit/:id"
+//             element={
+//               <ProtectedRoute loggedUser={loggedUser}>
+//                 <PedidoAdminEdit />
+//               </ProtectedRoute>
+//             }
+//           />
+
+
+//           <Route
+//             exact
+//             path="/product/buy/:id"
+//             element={<ProductDetails URL={URL} />}
+//           />
+//           <Route
+//             exact
+//             path="/pedidos"
+//             element={<PedidosTable />}
+//           />
+//           <Route
+//             exact
+//             path="/auth/login/"
+//             element={<Login setLoggedUser={setLoggedUser} />}
+//           />
+//           <Route exact path="/auth/register/" element={<Register setLoggedUser={setLoggedUser} />} />
+//           <Route exact path="*" element={<Error404 />} />
+//         </Routes>
+//       </main>
+//       {/* <enviarmail/> */}
+//       <Footer />
+//     </BrowserRouter>
+//   </div >
+// );
+
+
+
