17b1eb8de2b929d929a1f033ef8c3ab625ba8750
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-04-07 20:56:48 -0300

    correcion spinner

--- src/components/views/home/Home.js
+++ src/components/views/home/Home.js
@@ -52,7 +52,7 @@ const Home = ({ products, spinner }) => {
       <CarouselHome />
       <AboutUs />
 
-      {!spinner ? (
+      {spinner ? (
         <div className="text-center fw-bold fs-4 d-flex justify-content-center align-items-center" style={{"padding": "150px 150px"}}>
         <div className="spinner-border me-2" role="status">
           <span className="sr-only">Loading...</span>
