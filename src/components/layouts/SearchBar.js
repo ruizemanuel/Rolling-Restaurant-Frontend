0134e27d8a8ddb8ff1aebf193d0b313b44294cbf
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-04-08 22:29:17 -0300

    se agrego evento de click para que se cierre la barra de busqueda cuando el usuario accede a un producto desde ella

--- src/components/layouts/SearchBar.js
+++ src/components/layouts/SearchBar.js
@@ -36,11 +36,11 @@ function SearchBar({ placeholder, data }) {
                 />
                 <div className="searchIcon">
                     {filteredData.length === 0 ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                         </svg>
                     ) : (
-                        <button type="button" class="btn-close" aria-label="Close" onClick={clearInput}></button>
+                        <button type="button" className="btn-close" aria-label="Close" onClick={clearInput}></button>
                     )}
                 </div>
             </div>
@@ -50,7 +50,7 @@ function SearchBar({ placeholder, data }) {
                         return (
                             <Link
                                 to={`/product/buy/${value._id}`}
-                                state={{ borrar: clearInput }}
+                                onClick={clearInput}
                                 className="dataItem"
                             >
                                 {value.productName}
